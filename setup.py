834dbabdcd0d64604d4bccac5853f127ecfe2a11
protellus <129015214+protellus@users.noreply.github.com>
2025-03-07 18:13:09 -0500

    Versions again

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         "certifi==2025.1.31",
         "charset-normalizer==3.4.1",
         "Django==3.2",
+        "djangorestframework==3.14.0",
         "idna==3.10",
         "requests==2.32.3",
         "sqlparse==0.5.3",
