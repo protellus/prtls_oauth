bcd1cc5675f364751d4c4e4a0cdeed7e9f6764bc
protellus <129015214+protellus@users.noreply.github.com>
2025-03-08 08:31:42 -0500

    Correct version of putils

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "Django==3.2",
         "djangorestframework==3.14.0",
         "idna==3.4",
-        "putils>=0.2.3",
+        "prtls_utils @ git+https://github.com/protellus/prtls-utils.git@main",
         "pytz==2023.3",
         "requests==2.31.0",
         "sqlparse==0.4.4",
