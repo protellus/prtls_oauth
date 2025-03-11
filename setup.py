e2ab0738c5db46020a46fdf27ac539f11a7f6648
protellus <129015214+protellus@users.noreply.github.com>
2025-03-11 17:51:09 -0400

    Loosen requirements on utils

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "Django==3.2",
         "djangorestframework==3.14.0",
         "idna==3.4",
-        "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@0.1.5",
+        "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@main",
         "pytz==2023.3",
         "requests==2.31.0",
         "sqlparse==0.4.4",
