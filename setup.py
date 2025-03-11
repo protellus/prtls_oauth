e41d24839b14ac2c775de0aa872e3f4b0405df40
protellus <129015214+protellus@users.noreply.github.com>
2025-03-11 17:11:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "Django==3.2",
         "djangorestframework==3.14.0",
         "idna==3.4",
-        "prtls_utils @ git+https://github.com/protellus/prtls-utils.git@main",
+        "prtls_utils @ git+https://github.com/protellus/prtls-utils.git@0.1.5",
         "pytz==2023.3",
         "requests==2.31.0",
         "sqlparse==0.4.4",
