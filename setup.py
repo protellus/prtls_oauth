f715dba71de4887cc6d284d82ea4f7f5846066f2
protellus <129015214+protellus@users.noreply.github.com>
2025-03-12 17:30:05 -0400

    Fixing versions

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages=find_packages(include=["prtls_oauth", "prtls_oauth.*"]),  # ✅ Includes all package modules
     include_package_data=True,  # ✅ Ensures migrations and templates are included
     install_requires=[
-        "asgiref==3.7.2",
+        "asgiref==3.8.1",
         "certifi==2023.7.22",
         "charset-normalizer==3.2.0",
-        "Django==3.2",
+        "Django==5.1.7",
         "djangorestframework==3.14.0",
         "idna==3.4",
         "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@main",
