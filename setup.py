681267eb64a48a933b19c00446ea205bd35d54f7
protellus <129015214+protellus@users.noreply.github.com>
2025-03-12 20:19:16 -0400

    LICENSE and security patches

--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="prtls_oauth",
-    version="1.0.21",
-    packages=find_packages(include=["prtls_oauth", "prtls_oauth.*"]),  # ✅ Includes all package modules
-    include_package_data=True,  # ✅ Ensures migrations and templates are included
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    packages=find_packages(include=["prtls_oauth", "prtls_oauth.*"]), 
+    include_package_data=True, 
     install_requires=[
         "asgiref==3.8.1",
-        "certifi==2024.7.4",
+        "certifi==2023.7.2",
         "charset-normalizer==3.2.0",
-        "Django==4.2.20",
-        "djangorestframework==3.15.2",
-        "idna==3.7",
+        "Django>=4.2.20,<5.1",
+        "djangorestframework==3.14.0",
+        "idna==3.4",
         "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@main",
         "pytz==2023.3",
-        "requests==2.32.2",
-        "sqlparse==0.5.0",
+        "requests==2.32.0",
+        "sqlparse==0.5.3",
         "tzdata==2025.1",
-        "urllib3==1.26.19"
+        "urllib3==1.26.16"
     ],
     extras_require={
         "dev": [
