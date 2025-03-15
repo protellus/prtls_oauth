e040c3b5e735a07600af13d0903331d5c2c102f4
protellus <129015214+protellus@users.noreply.github.com>
2025-03-15 13:24:53 -0400

    Loosen up dependency requirements

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ setup(
     packages=find_packages(include=["prtls-oauth", "prtls-oauth.*"]), 
     include_package_data=True, 
     install_requires=[
-        "asgiref==3.8.1",
-        "certifi==2023.7.2",
-        "charset-normalizer==3.2.0",
+        "asgiref>=3.8.1",
+        "certifi>=2023.7.2",
+        "charset-normalizer>=3.2.0",
         "Django>=4.2.20,<5.1",
-        "djangorestframework==3.14.0",
-        "idna==3.4",
+        "djangorestframework>=3.14.0",
+        "idna>=3.4",
         "prtls-utils @ git+https://github.com/protellus/prtls-utils.git@main",
-        "pytz==2023.3",
-        "requests==2.32.0",
-        "sqlparse==0.5.3",
-        "tzdata==2025.1",
-        "urllib3==1.26.16"
+        "pytz>=2023.3",
+        "requests>=2.32.0",
+        "sqlparse>=0.5.3",
+        "tzdata>=2025.1",
+        "urllib3>=1.26.16"
     ],
     extras_require={
         "dev": [
